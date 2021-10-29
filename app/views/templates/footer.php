32f1e70ecd2bf13685e9eb5295194564b49fa431
Zaenal Arif <92130191+zenrif@users.noreply.github.com>
2021-10-29 22:21:24 +0700

    Update footer.php

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -8,7 +8,6 @@
     }
 </script>
 <script src="<?= BASEURL; ?>/public/js/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="<?= BASEURL; ?>/public/js/bootstrap.js"> </script>
 <script src="<?= BASEURL; ?>/public/js/script.js"> </script>
 </body>
